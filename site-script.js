0376dd93e11940e58e126bc9139643f1d3c30d7f
Cooner3ja <164369194+Cooner3ja@users.noreply.github.com>
2025-04-05 15:37:25 -0400

    Update site-script.js

--- site-script.js
+++ site-script.js
@@ -243,7 +243,7 @@ function initServiceCarousel() {
 document.addEventListener("DOMContentLoaded", function () {
     const menuIcon = document.querySelector(".icon");
     const menuLinks = document.getElementById("myLinks");
-});
+    
     menuIcon.addEventListener("click", function () {
         menuLinks.classList.toggle("show");
     });
@@ -254,3 +254,4 @@ document.addEventListener("DOMContentLoaded", function () {
             menuLinks.classList.remove("show");
         }
     });
+});
