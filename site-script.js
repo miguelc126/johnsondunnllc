5f49bac531bbcd76799284f05cad2d46b7aecb7a
miguelc126 <miguelcarredondo126@gmail.com>
2025-05-14 21:09:51 -0400

    Add files via upload

--- site-script.js
+++ site-script.js
@@ -276,6 +276,7 @@ function initServiceCarousel() {
 function initObfu() {
     let e = String.fromCharCode(106,111,104,110,115,111,110,46,100,117,110,110,108,108,99,64) + '&#8203;' + String.fromCharCode(103,109,97,105,108,46,99,111,109);
     document.getElementById('eml').innerHTML = e;   
+    document.getElementById('footeml').innerHTML = "<i class='fas fa-envelope'></i>     " + e;
 }
 function initModals() {
     let accessibilityModal = document.getElementById('accessibility-modal');
